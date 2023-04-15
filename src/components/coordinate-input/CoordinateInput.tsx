522a26ef10d56c7cb137613d1a8355471811fc60
PedroPerpetua <pedro.perpetua@sapo.pt>
2023-04-15 18:24:34 +0100

    Fixed missing css

--- src/components/coordinate-input/CoordinateInput.tsx
+++ src/components/coordinate-input/CoordinateInput.tsx
@@ -1,6 +1,7 @@
 import { Typography } from '@mui/material';
 import { Point } from '../../types';
 import SingleCoordinateInput from './SingleCoordinateInput';
+import './CoordinateInput.scss';
 
 
 type CoordinateInputProps = {
