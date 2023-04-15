55907fb2839e69b8bcee61f54690a48f810c44ae
PedroPerpetua <pedro.perpetua@sapo.pt>
2023-04-15 09:55:53 +0100

    Added SASS

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import './index.css';
+import './index.scss';
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
