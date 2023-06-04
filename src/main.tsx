de726463bfcb3e1fd400bd8e5460cfbd83d106aa
PedroPerpetua <pedro.perpetua@sapo.pt>
2023-06-04 03:54:15 +0100

    Cleared website and set up title and icon

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'react-image-crop/src/ReactCrop.scss';
+import { RecoilRoot } from 'recoil';
 
 import App from './App';
 
@@ -9,9 +9,12 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import './index.scss';
+import 'react-image-crop/src/ReactCrop.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
   </React.StrictMode>,
 );
